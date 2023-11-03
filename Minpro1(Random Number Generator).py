8474a1d05dcdf0e4d93ce5b6528072ecd2c4c132
fadilwiharto <126900179+fadilwiharto@users.noreply.github.com>
2023-11-03 13:13:34 +0700

    Add files via upload

---
+++ Minpro1(Random Number Generator).py
@@ -0,0 +1,30 @@
+import random
+
+min_value = 1
+
+def generator(max_value):
+    number = random.randint(min_value, max_value)
+    return number
+
+def add_maximum_number(max_value):
+    result = []
+    for number in range(1, max_value+1):
+        result.append(number)
+    return result
+
+def main():
+    while True:
+        print(f"Press Enter to generate a random number. (type Q to quit)")
+        user_input = input()
+        if user_input.lower() == 'q':
+            break
+        max_value = int(input("Please enter the maximum value: "))
+        generated_number = generator(max_value)  # Pass the max_value as an argument
+        add_maximum_number(max_value)
+        print(f"Generated Random Number: {generated_number}")
+
+main()
+
+
+
+
